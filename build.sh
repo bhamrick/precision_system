d8f6670d4ef3a3ff04c5c0b8e713d2704bd9e0f4
Brian Hamrick <brian.c.hamrick@gmail.com>
2022-07-27 04:42:11 -0400

    Move relay to a secondary source file and build a table of contents

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 pandoc -f markdown -t gfm -o basic.md --toc -s $(cat contents-basic.txt)
 pandoc -f markdown -t gfm -o open.md --toc -s $(cat contents-open.txt)
+pandoc -f markdown -t gfm -o relay.md --toc -s 90-relay.md
